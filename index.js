3c07869bb95307a71bd2b90cfbe3df8aad92c466
PatrickEinstein <mohammedola1234@gmail.com>
2023-02-26 22:01:51 +0100

    done

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const url = "mongodb+srv://admin:admin@cluster0.cfeljvx.mongodb.net/user-login";
 
-// mongoose
-//   .connect(url)
-//   .then(() => {
-//     console.log("Connected to MongoDB successfully");
-//   })
-//   .catch((err) => {
-//     console.log("Error connecting to MongoDB", err);
-//   });
-connectDb();
+
 const app = express();
-
-const port = process.env.PORT || 7000;
-
 app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
+
+
+  const MONGOSE_URL = "mongodb+srv://admin:admin@cluster0.cfeljvx.mongodb.net/optice-login";
+
+  mongoose.connect(MONGOSE_URL)
+  .then( ( ) =>( console.log("Mongo Db connected sucessfully")))
+  .catch(( error) => ( console.log(`error => ${error}`)))
+
+
+const port = process.env.PORT || 7000;
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
