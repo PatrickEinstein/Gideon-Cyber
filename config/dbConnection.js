29acfa85d002fc78cd7c03e5b4cd081ca85b7dc6
PatrickEinstein <mohammedola1234@gmail.com>
2023-02-26 21:47:23 +0100

    gideon

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,15 @@
 const { default: mongoose } = require("mongoose");
 
 const connectDb = async () => {
+
+  const MONGOSE_URL = "mongodb+srv://admin:admin@cluster0.cfeljvx.mongodb.net/optice-login";
   try {
-    const connect = await mongoose.connect(process.env.MONGOSE_URL);
+    const connect = await mongoose.connect(MONGOSE_URL);
     console.log(
       "MongoDB connected",
-      connect.connection.host,
-      connect.connection.port
-    );
+    )
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.log(`there is an error ${error}`)
   }
 };
 
